ba93cfd9b3cd5ef2e7604b5c0b9fc9b74ba287ec
AQ1011 <anhquocngo1011@gmail.com>
2021-05-07 21:24:10 +0700

    fix

--- app/src/main/java/com/example/myapplication/ui/changePassword/ChangePasswordFragment.java
+++ app/src/main/java/com/example/myapplication/ui/changePassword/ChangePasswordFragment.java
@@ -1,5 +1,7 @@
 package com.example.myapplication.ui.changePassword;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +19,8 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import com.example.myapplication.R;
+import com.example.myapplication.database.AppDatabase;
+import com.example.myapplication.database.User;
 
 public class ChangePasswordFragment extends Fragment {
 
@@ -30,6 +35,15 @@ public class ChangePasswordFragment extends Fragment {
         final Button btn_change;
         final Button btn_home;
 
+        AppDatabase db = AppDatabase.getDatabase(getContext());
+
+        //get username
+        SharedPreferences sharedPref = getContext().getSharedPreferences("NOTE_SHARED_PREFERENCE"
+                , Context.MODE_PRIVATE);
+
+        String username = sharedPref.getString("username","Default name");
+
+
         currentPass = (EditText)root.findViewById(R.id.ed_current_password);
         newPass = (EditText)root.findViewById(R.id.ed_new_password);
         confirmPass = (EditText)root.findViewById(R.id.ed_confirm_password);
@@ -38,6 +52,32 @@ public class ChangePasswordFragment extends Fragment {
 
         btn_home.setOnClickListener(v -> {
             Navigation.findNavController(root).navigate(R.id.action_changePasswordFragment_to_nav_home);
+        });
+
+        btn_change.setOnClickListener(v -> {
+            db.getQueryExecutor().execute(() -> {
+                User usr = db.userDao().loadByEmail(username);
+                String pass = currentPass.getText().toString();
+                if(pass != usr.password){
+                    getActivity().runOnUiThread(()->{
+                        currentPass.setError("Mật khẩu hiện tại không đúng");
+                    });
+                    return;
+                }
+                if(confirmPass.getText().toString() != newPass.getText().toString()){
+                    getActivity().runOnUiThread(()->{
+                        confirmPass.setError("Mật khẩu không giống");
+                    });
+                    return;
+                }
+                usr.password = newPass.getText().toString();
+                db.userDao().update(usr);
+                getActivity().runOnUiThread(()->{
+                    Toast.makeText(getContext(), "Đổi mật khẩu thành công", Toast.LENGTH_SHORT).show();
+                    Navigation.findNavController(root).navigate(R.id.action_changePasswordFragment_to_nav_home);
+                });
+            });
+
         });
 
         currentPass.setText("");
